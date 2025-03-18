ba98231fa93bc6791166b7f6733e895a4b962f21
gigumay <gigumay@gmail.com>
2025-03-18 02:06:37 +0100

    Added option to pass state dict file as param

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -627,6 +627,11 @@ class Model(nn.Module):
         self.trainer = (trainer or self._smart_load("trainer"))(overrides=args, _callbacks=self.callbacks)
         if not args.get("resume"):  # manually set model only if not resuming
             self.trainer.model = self.trainer.get_model(weights=self.model if self.ckpt else None, cfg=self.model.yaml)
+            
+            if kwargs["state"] is not None:
+                state = torch.load(kwargs["state"])
+                self.trainer.model.load_state_dict(state, strict=True)
+
             self.model = self.trainer.model
 
             if SETTINGS["hub"] is True and not self.session:
